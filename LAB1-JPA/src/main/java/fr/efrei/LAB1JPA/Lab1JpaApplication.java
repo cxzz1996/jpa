db06e8b8d646f7e28d335ed94e943efda6a26ef4
cxzz1996 <37656901+cxzz1996@users.noreply.github.com>
2019-11-16 21:25:58 +0100

    Add files via upload

---
+++ LAB1-JPA/src/main/java/fr/efrei/LAB1JPA/Lab1JpaApplication.java
@@ -0,0 +1,13 @@
+package fr.efrei.LAB1JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1JpaApplication.class, args);
+	}
+
+}
---
+++ LAB1-JPA/src/test/java/fr/efrei/LAB1JPA/Lab1JpaApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.efrei.LAB1JPA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lab1JpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
